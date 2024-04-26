d760600926486480a80915290a10ce08c4d755af
Matthew Colson <mcolson@arizona.edu>
2024-04-26 15:58:51 -0700

    add pfaas endpoints and fix dijkstras

--- PFaaS/pathfind_test.go
+++ PFaaS/pathfind_test.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"testing"
 )
 
-func TestPathfind(t *testing.T) {
+func TestPathfind1(t *testing.T) {
 	t.Parallel()
 
 	//       1
@@ -30,7 +28,92 @@ func TestPathfind(t *testing.T) {
 	}...)
 
 	out := Dijkstra(graph, 1, 8)
-	fmt.Println(out)
+	t.Log(out)
 
-	log.Fatal("test")
+	if len(out) != 4 {
+		t.Fatalf("expected 4 nodes, got %d", len(out))
+	}
+
+	exp := []NodeID{8, 6, 5, 3}
+	for i, node := range out {
+		if node != exp[i] {
+			t.Fatalf("expected node %d to be %d, got %d", i, exp[i], node)
+		}
+	}
+}
+
+func TestPathfind2(t *testing.T) {
+	t.Parallel()
+
+	//       1
+	//      / \
+	//      2  3
+	//     /| / \
+	//    4 5   7
+	//    \ |   |
+	//     6 - 8
+	graph := NewGraph([]Link{
+		{1, 2, 2},
+		{1, 2, 3},
+		{2, 1, 4},
+		{2, 3, 5},
+		{3, 1, 5},
+		{3, 2, 7},
+		{4, 1, 6},
+		{5, 3, 6},
+		{6, 1, 8},
+		{7, 6, 8},
+	}...)
+
+	out := Dijkstra(graph, 1, 8)
+	t.Log(out)
+
+	if len(out) != 4 {
+		t.Fatalf("expected 4 nodes, got %d", len(out))
+	}
+
+	exp := []NodeID{8, 6, 4, 2}
+	for i, node := range out {
+		if node != exp[i] {
+			t.Fatalf("expected node %d to be %d, got %d", i, exp[i], node)
+		}
+	}
+}
+
+func TestPathfind3(t *testing.T) {
+	t.Parallel()
+
+	//       1
+	//      / \
+	//      2  3
+	//     /| / \
+	//    4 5   7
+	//    \ |   |
+	//     6 - 8
+	graph := NewGraph([]Link{
+		{1, 2, 2},
+		{1, 2, 3},
+		{2, 1, 4},
+		{2, 3, 5},
+		{3, 1, 5},
+		{3, 1, 7},
+		{4, 1, 6},
+		{5, 3, 6},
+		{6, 1, 8},
+		{7, 1, 8},
+	}...)
+
+	out := Dijkstra(graph, 1, 8)
+	t.Log(out)
+
+	if len(out) != 3 {
+		t.Fatalf("expected 3 nodes, got %d", len(out))
+	}
+
+	exp := []NodeID{8, 7, 3}
+	for i, node := range out {
+		if node != exp[i] {
+			t.Fatalf("expected node %d to be %d, got %d", i, exp[i], node)
+		}
+	}
 }
